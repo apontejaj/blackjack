73d66ec60c4bf4c2a4b5cb5adcfa2b5d823ec389
Amilcar Aponte <apontejaj@gmail.com>
2017-08-08 23:12:19 +0100

    More Changes
    - Inclusion of two boolean "busted" and "blackjack" to identify if the
    player of the dealer has a blackjack or has busted.
    - Including global integer to determining the bet amount
    - the function playing is now for players turn.
    - there is an extra funtion to wrap up the whole game

--- blackjack.py
+++ blackjack.py
@@ -12,6 +12,8 @@ class Player(object):
         self.name = name
         self.balance = balance
         self.hand = []
+        self.busted = False
+        self.blackjack = False
 
     def __str__(self):
         return "[ {}, your balance is {} ]".format(self.name, self.balance)
@@ -29,50 +31,48 @@ class Card(object):
 class Dealer(object):
     def __init__(self):
         self.hand = []
+        self.busted = False
+        self.blackjack = False
 
+bet = 0
 deck = []
 def creating_deck():
+    global deck
+    deck = []
     suits = ["HEARTS", "CLUBS", "SPADES", "DIAMONS"]
     ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack",
          "Queen", "King"]
     for suit in suits:
         for rank in ranks:
             deck.append(Card(suit, rank))
-creating_deck()
 
 asking_user = lambda question: raw_input(question)
 
 player = None
 dealer = None
-def playing():
-    global player
-    global dealer
-    player = Player(asking_user("Type in your name"),1000)
-    dealer = Dealer()
-    print ("Welcome {}, let's start playing".format(player.name))
 
-    hitting(player)
+def playing():
+    print ("Welcome {}, let's start playing".format(player.name))
     hitting(player)
     hitting(dealer)
+    hitting(player)
     hitting(dealer)
     display_hand(player)
 
     keep_playing = True
-    if counting() == 21:
-        print ("Your score is {}".format(counting()))
+    if counting(player) == 21:
         print ("BLACKJACK")
         keep_playing = False
+        player.blackjack = True
+        
 
     while keep_playing:
         valid_answer = False
-        busted = False
-        print ("Your score is {}".format(counting()))
-        if counting() > 21:
-            print ("You've busted - This is an automatic loss")
-            busted = True
+        if counting(player) > 21:
+            player.busted = True
             keep_playing = False
         
-        while not valid_answer and not busted:
+        while not valid_answer and not player.busted:
             answer = asking_user("Press H to hit or S to stay").lower()
             if (answer == "h" or answer == "s"):
                 valid_answer = True
@@ -84,18 +84,16 @@ def playing():
                     keep_playing = False
             else:
                 print ("Not a valid answer")
+
+    dealer_turn()
+    winning()
         
 def hitting(turn):
-    global player
-    global dealer
     got_card = False
     while not got_card:
         card = random.randint(0,51)
         if deck[card].available:
-            if turn == player:
-                player.hand.append(card)
-            if turn == dealer:
-                dealer.hand.append(card)
+            turn.hand.append(card)
             deck[card].available = False
             got_card = True
             
@@ -106,17 +104,19 @@ def display_hand(turn):
         print("Your hand is:")
         for card in player.hand:
             print (deck[card])
+        print ("Your score is {}".format(counting(player)))
     if turn == dealer:
         print ("This is my hand")
         for card in dealer.hand:
             print (deck[card])
+        print ("My score is {}".format(counting(dealer)))
         
-def counting():
+def counting(turn):
     score = 0
-    for card in player.hand:
+    for card in turn.hand:
         score += value(card)
     if score > 21:
-        for card in player.hand:
+        for card in turn.hand:
             if deck[card].rank == "Ace":
                 if deck[card].highest_value:
                     score = score - 10
@@ -141,4 +141,91 @@ def value(card):
             val = 1
     return val
 
-playing()
+def dealer_turn():
+    staying = False
+    while not staying and not player.busted:
+        display_hand(dealer)
+        if (counting(dealer) > 16 and counting(dealer) < 21):
+            print ("I decide to stay")
+            staying = True
+        elif counting(dealer) < 17:
+            print ("I decide to hit")
+            hitting(dealer)
+        elif counting(dealer) > 21:
+            print ("I busted")
+            staying = True
+            dealer.busted = True
+        elif counting(dealer) == 21:
+            print ("I have a 21!")
+            staying = True
+            dealer.blackjack = True
+
+def winning():
+    global bet
+    if dealer.busted and not player.busted:
+        print ("I busted, you win")
+        player.balance += (2 * bet)
+    elif player.busted and not dealer.busted:
+        print ("You busted, that's an automatic loss, I win")
+    elif dealer.blackjack and player.blackjack:
+        print ("We're even")
+        player.balance += bet
+    elif player.blackjack and not dealer.blackjack:
+        print ("Solid blackjack! You win")
+        player.balance += (2.5 * bet)
+    elif dealer.blackjack and not player.blackjack:
+        print ("Sorry, dude. Blackjack never loses, you do!")
+    elif counting(dealer) > counting (player):
+        print ("I win, cause I'm closer to 21")
+    elif counting(player) > counting(dealer):
+        print ("You win, cause you're closer to 21")
+        player.balance += (2 * bet)
+    bet = 0
+    
+def betting():
+    global bet
+    valid_bet = False
+    while not valid_bet:
+        try:
+            bet = int(asking_user("How much do you want to bet?"))
+        except:
+            print("That is not a valid bet")
+        else:
+            valid_bet = True
+    player.balance = player.balance - bet
+    
+
+def begining():
+    global player
+    global dealer
+    player = Player(asking_user("Type in your name"),1000)
+    dealer = Dealer()
+    another_game = True
+    
+    while another_game:
+        player.hand = []
+        player.busted = False
+        player.blackjack = False
+        dealer.hand = []
+        dealer.busted = False
+        dealer.blackjack = False
+
+        betting()      
+        creating_deck()
+        playing()
+
+        valid_answer = False
+        while not valid_answer:
+            answer = asking_user("Do you want to play again?").lower()
+            if answer == "y":
+                print("Lets play again")
+                valid_answer = True
+            elif answer == "n":
+                print("This is over")
+                valid_answer = True
+                another_game = False
+            else:
+                print("I need a valid answer, either Y or N")
+
+begining()
+print (player.balance)
